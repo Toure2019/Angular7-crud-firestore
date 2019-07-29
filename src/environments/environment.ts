59fe15fbc7ac851543cc0029c7f5c546bb0f77c3
Toure2019 <51404116+Toure2019@users.noreply.github.com>
2019-07-29 16:49:26 +0200

    First commit: petit projet complet pour l'ajout, la modification et la suppression avec AngularFire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseConfig : {
+    apiKey: "AIzaSyA04gw4A40w3fi73vcs1F-E_IJDamIQYOs",
+    authDomain: "motivator-24172.firebaseapp.com",
+    databaseURL: "https://motivator-24172.firebaseio.com",
+    projectId: "motivator-24172",
+    storageBucket: "motivator-24172.appspot.com",
+    messagingSenderId: "577638658757",
+    appId: "1:577638658757:web:88c2dc103158db55"
+  }
 };
 
 /*
